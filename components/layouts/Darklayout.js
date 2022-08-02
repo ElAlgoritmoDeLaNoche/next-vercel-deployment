e7b018c7a99bac7c0a627d66cf78ff93d67050a8
Francisco Navarrete <francisconavarrete@MacBook-Air-de-Francisco.local>
2022-08-02 02:38:54 -0500

    01-initial-demo

---
+++ components/layouts/Darklayout.js
@@ -0,0 +1,16 @@
+export const Darklayout = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+        borderRadius: '5px',
+        padding: '10px'
+      }}
+    >
+      <h3>Darklayout</h3>
+      <div>
+        {children}
+      </div>
+    </div>
+  )
+}
